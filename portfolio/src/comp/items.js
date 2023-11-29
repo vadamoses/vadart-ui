a2a380ba704b8ad9bf1978029b0f3923d6f9687c
Victor Moses <2050610@cognizant.com>
2023-11-29 16:49:12 +0100

    streamlining item storage and display

---
+++ portfolio/src/comp/items.js
@@ -0,0 +1,28 @@
+import images from '../images';
+
+
+const items = [
+    {
+      id: 1,
+      background: images[5],
+      name: 'Item 1',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel sapien arcu. Donec sapien eros, efficitur blandit dui vitae, imperdiet consectetur nibh.',
+    },
+    {
+      id: 2,
+      background: images[4],
+      name: 'Item 2',
+      description:
+        'Nunc efficitur tincidunt malesuada. Pellentesque blandit sapien sed orci tristique molestie. Donec ut metus a sapien gravida convallis sed maximus neque. Aliquam consequat fringilla porta. Curabitur eget semper tortor.',
+    },
+    {
+      id: 3,
+      background: images[6],
+      name: 'Item 3',
+      description:
+        'Nunc efficitur tincidunt malesuada. Pellentesque blandit sapien sed orci tristique molestie. Donec ut metus a sapien gravida convallis sed maximus neque. Aliquam consequat fringilla porta. Curabitur eget semper tortor.',
+    },
+  ];
+
+  export default items;
--- portfolio/src/comp/work.js
+++ portfolio/src/comp/work.js
@@ -1,7 +1,8 @@
 import { React, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
-import images from '../images';
+// import images from "../images";
+import items from "./items";
 import "./work.css";
 
 const Work = () => {
@@ -25,173 +26,58 @@ const Work = () => {
 				<div className="top_box">
 					<p>latest works and featured</p>
 				</div>
-				<div className="container">
-					<div className="left">
-						<div className="exp-card-container">
-							<motion.div
-								className="exp-card"
-								id="card1"
-								//onHoverStart={(e) => handleClick(e)}
-								onClick={(e) => handleClick(e)}
-								transition={{ layout: { duration: 1.2, type: "spring" } }}
-								style={{
-									borderRadius: ".8rem",
-									boxShadow: "0px 10px 30px rgba(249, 242, 242,0.4)",
-								}}
-								layout
-							>
-								<motion.h2 layout="position">Item Details.</motion.h2>
-								{isOpen && divId.match("card1") && (
+				<div>
+					{items.map((item) => (
+						<div className="container">
+							{/* {items.length %2 === 0 ? "left" :"right" } */}
+							<div className="left">
+								<div className="exp-card-container">
 									<motion.div
-										className="exp-card-content"
-										initial={{ opacity: 0 }}
-										animate={{ opacity: 1 }}
-										transition={{ duration: 1 }}
-										exit={{ opacity: 0 }}
+										className="exp-card"
+										id={item.id}
+										//onHoverStart={(e) => handleClick(e)}
+										onClick={(e) => handleClick(e)}
+										transition={{ layout: { duration: 1.2, type: "spring" } }}
+										style={{
+											borderRadius: ".8rem",
+											boxShadow: "0px 10px 30px rgba(249, 242, 242,0.4)",
+										}}
+										layout
 									>
-										<p>
-											Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-											Error dolorum praesentium vero rem. Voluptates, quibusdam
-											architecto explicabo provident cumque dicta.
-										</p>
-										<p>
-											Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-											Consectetur, sunt.
-										</p>
-										<button type="submit" onClick={routeChange}>
-											Buy Now
-										</button>
+										<motion.h2 layout="position">Item Details.</motion.h2>
+										{isOpen && divId.match(item.id) && (
+											<motion.div
+												className="exp-card-content"
+												initial={{ opacity: 0 }}
+												animate={{ opacity: 1 }}
+												transition={{ duration: 1 }}
+												exit={{ opacity: 0 }}
+											>
+												<p> {item.description} </p>
+												<button type="submit" onClick={routeChange}>
+													Buy Now
+												</button>
+											</motion.div>
+										)}
 									</motion.div>
-								)}
-							</motion.div>
-						</div>
-					</div>
-					<div className="right">
-						<div className="content">
-							<motion.div
-								className="box"
-								transition={{ layout: { duration: 1.2, type: "spring" } }}
-								layout
-							>
-								<motion.div className="img_box">
-									<img src={images[5]} alt="Artpiece"></img>
-								</motion.div>
-								<p>mobile designing</p>
-							</motion.div>
-						</div>
-					</div>
-				</div>
-				<div className="container">
-                <div className="right">
-						<div className="content">
-							<motion.div
-								className="box"
-								transition={{ layout: { duration: 1.2, type: "spring" } }}
-								layout
-							>
-								<motion.div className="img_box">
-									<img src={images[4]} alt="project"></img>
-								</motion.div>
-								<p>mobile designing</p>
-							</motion.div>
-						</div>
-					</div>
-					<div className="left">
-						<div className="exp-card-container">
-							<motion.div
-								className="exp-card"
-								id="card2"
-								//onHoverStart={(e) => handleClick(e)}
-								onClick={(e) => handleClick(e)}
-								transition={{ layout: { duration: 1.2, type: "spring" } }}
-								style={{
-									borderRadius: ".8rem",
-									boxShadow: "0px 10px 30px rgba(249, 242, 242,0.4)",
-								}}
-								layout
-							>
-								<motion.h2 layout="position">Item Details.</motion.h2>
-								{isOpen && divId.match("card2") && (
+								</div>
+							</div>
+							<div className= "right">
+								<div className="content">
 									<motion.div
-										className="exp-card-content"
-										initial={{ opacity: 0 }}
-										animate={{ opacity: 1 }}
-										transition={{ duration: 1 }}
-										exit={{ opacity: 0 }}
+										className="box"
+										transition={{ layout: { duration: 1.2, type: "spring" } }}
+										layout
 									>
-										<p>
-											Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-											Error dolorum praesentium vero rem. Voluptates, quibusdam
-											architecto explicabo provident cumque dicta.
-										</p>
-										<p>
-											Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-											Consectetur, sunt.
-										</p>
-										<button type="submit" onClick={routeChange}>
-											Buy Now
-										</button>
+										<motion.div className="img_box">
+											<img src={item.background} alt="Artpiece"></img>
+										</motion.div>
+										<p>{item.name}</p>
 									</motion.div>
-								)}
-							</motion.div>
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-				<div className="container">
-					<div className="left">
-						<div className="exp-card-container">
-							<motion.div
-								className="exp-card"
-								id="card3"
-								//onHoverStart={(e) => handleClick(e)}
-								onClick={(e) => handleClick(e)}
-								transition={{ layout: { duration: 1.2, type: "spring" } }}
-								style={{
-									borderRadius: ".8rem",
-									boxShadow: "0px 10px 30px rgba(249, 242, 242,0.4)",
-								}}
-								layout
-							>
-								<motion.h2 layout="position">Item Details.</motion.h2>
-								{isOpen && divId.match("card3") && (
-									<motion.div
-										className="exp-card-content"
-										initial={{ opacity: 0 }}
-										animate={{ opacity: 1 }}
-										transition={{ duration: 1 }}
-										exit={{ opacity: 0 }}
-									>
-										<p>
-											Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-											Error dolorum praesentium vero rem. Voluptates, quibusdam
-											architecto explicabo provident cumque dicta.
-										</p>
-										<p>
-											Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-											Consectetur, sunt.
-										</p>
-										<button type="submit" onClick={routeChange}>
-											Buy Now
-										</button>
-									</motion.div>
-								)}
-							</motion.div>
-						</div>
-					</div>
-					<div className="right">
-						<div className="content">
-							<motion.div
-								className="box"
-								transition={{ layout: { duration: 1.2, type: "spring" } }}
-								layout
-							>
-								<motion.div className="img_box">
-									<img src={images[6]} alt="project"></img>
-								</motion.div>
-								<p>mobile designing</p>
-							</motion.div>
-						</div>
-					</div>
+					))}
 				</div>
 			</div>
 		</>
